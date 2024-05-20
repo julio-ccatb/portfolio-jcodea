939dbf29298d860db7e4f9b73f245f2826f017de
Julio Castaño <julio_ccatb@hotmail.com>
2024-05-20 11:12:19 -0400

    To get resume 1

--- src/app/_components/modeToggle.tsx
+++ src/app/_components/modeToggle.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import * as React from "react";
 import { useTheme } from "next-themes";
 
-import { MoonIcon, SunIcon } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import {
   DropdownMenu,
@@ -10,14 +10,21 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useScroll } from "framer-motion";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
+  const { scrollYProgress } = useScroll();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          className={`${scrollYProgress.get() > 0.9 && "hidden"}`}
+          variant="outline"
+          size="icon"
+        >
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
